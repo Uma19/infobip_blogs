995ded47acf3a9a590049422e3b2ad2193239488
Uma19 <umapuja@gmail.com>
2018-08-21 20:59:44 +0530

    Add files via upload
    added src files

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import './App';
+import './App.scss';
+import { Container } from './blogs';
+
+class App extends React.Component {
+  render() {
+    return (
+      <Container/>
+    );
+  }
+}
+
+export default App;
